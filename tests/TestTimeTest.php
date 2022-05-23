0dd7e9e510eee1cb34cdcff5a01236e70b320537
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2022-05-23 15:11:47 +0200

    apply strict assertions

--- tests/TestTimeTest.php
+++ tests/TestTimeTest.php
@@ -15,16 +15,34 @@ class TestTimeTest extends TestCase
         $now = Carbon::createFromFormat('Y-m-d H:i:s', '2019-01-02 03:44:55');
 
         TestTime::freeze($now);
-        $this->assertEquals('2019-01-02 03:44:55', (new Carbon())->format('Y-m-d H:i:s'));
+        $this->assertSame('2019-01-02 03:44:55', (new Carbon())->format('Y-m-d H:i:s'));
         $this->assertEquals('2019-01-02 03:44:55', (new CarbonImmutable())->format('Y-m-d H:i:s'));
 
         TestTime::addYear();
-        $this->assertEquals('2020', (new Carbon())->format('Y'));
+        $this->assertSame('2020', (new Carbon())->format('Y'));
         $this->assertEquals('2020', (new CarbonImmutable())->format('Y'));
 
         TestTime::addYear()->addYear();
-        $this->assertEquals('2022', (new Carbon())->format('Y'));
+        $this->assertSame('2022', (new Carbon())->format('Y'));
         $this->assertEquals('2022', (new CarbonImmutable())->format('Y'));
+    }
+
+    /** @test */
+    public function it_can_control_the_flow_of_time_with_carbon_immutable()
+    {
+        $now = CarbonImmutable::createFromFormat('Y-m-d H:i:s', '2019-01-02 03:44:55');
+
+        TestTime::immutable()::freeze($now);
+        $this->assertEquals('2019-01-02 03:44:55', (new Carbon())->format('Y-m-d H:i:s'));
+        $this->assertSame('2019-01-02 03:44:55', (new CarbonImmutable())->format('Y-m-d H:i:s'));
+
+        TestTime::addYear();
+        $this->assertEquals('2020', (new Carbon())->format('Y'));
+        $this->assertSame('2020', (new CarbonImmutable())->format('Y'));
+
+        TestTime::addYear()->addYear();
+        $this->assertEquals('2022', (new Carbon())->format('Y'));
+        $this->assertSame('2022', (new CarbonImmutable())->format('Y'));
     }
 
     /** @test */
@@ -36,7 +54,27 @@ class TestTimeTest extends TestCase
         sleep(2);
         $newTimestamp = Carbon::now()->timestamp;
 
-        $this->assertEquals($previousTimestamp, $newTimestamp);
+        $this->assertSame($previousTimestamp, $newTimestamp);
+
+
+        $previousTimestamp = CarbonImmutable::now()->timestamp;
+
+        sleep(2);
+        $newTimestamp = CarbonImmutable::now()->timestamp;
+
+        $this->assertSame($previousTimestamp, $newTimestamp);
+    }
+
+    /** @test */
+    public function it_will_freeze_the_current_time_by_passing_no_arguments_to_it_with_carbon_immutable()
+    {
+        TestTime::freeze();
+        $previousTimestamp = CarbonImmutable::now()->timestamp;
+
+        sleep(2);
+        $newTimestamp = CarbonImmutable::now()->timestamp;
+
+        $this->assertSame($previousTimestamp, $newTimestamp);
     }
 
     /** @test */
@@ -44,8 +82,8 @@ class TestTimeTest extends TestCase
     {
         TestTime::freeze('Y-m-d', '1979-09-22');
 
-        $this->assertEquals('1979-09-22', Carbon::now()->format('Y-m-d'));
-        $this->assertEquals('1979-09-22', CarbonImmutable::now()->format('Y-m-d'));
+        $this->assertSame('1979-09-22', Carbon::now()->format('Y-m-d'));
+        $this->assertSame('1979-09-22', CarbonImmutable::now()->format('Y-m-d'));
     }
 
     /** @test */
@@ -53,7 +91,8 @@ class TestTimeTest extends TestCase
     {
         TestTime::freezeAtSecond('Y-m-d H:i:s.u', '2019-01-02 03:44:55.667788');
 
-        $this->assertEquals('2019-01-02 03:44:55.000000', (new Carbon)->format('Y-m-d H:i:s.u'));
+        $this->assertSame('2019-01-02 03:44:55.000000', (new Carbon)->format('Y-m-d H:i:s.u'));
+        $this->assertSame('2019-01-02 03:44:55.000000', (new CarbonImmutable())->format('Y-m-d H:i:s.u'));
     }
 
     /** @test */
@@ -64,27 +103,33 @@ class TestTimeTest extends TestCase
 
         sleep(2);
         $newTimestamp = Carbon::now()->timestamp;
-        $this->assertEquals($previousTimestamp, $newTimestamp);
+        $this->assertSame($previousTimestamp, $newTimestamp);
 
         TestTime::unfreeze();
 
         sleep(2);
         $newTimestamp = Carbon::now()->timestamp;
         $this->assertNotEquals($previousTimestamp, $newTimestamp);
+
+
     }
 
     /** @test */
-    public function it_can_work_with_carbon_immutable()
+    public function it_can_unfreeze_time_with_carbon_immutable()
     {
-        $now = CarbonImmutable::createFromFormat('Y-m-d H:i:s', '2019-01-02 03:44:55');
+        TestTime::freeze();
+        $previousTimestamp = CarbonImmutable::now()->timestamp;
 
-        TestTime::immutable()::freeze($now);
-        $this->assertSame('2019-01-02 03:44:55', (new CarbonImmutable())->format('Y-m-d H:i:s'));
+        sleep(2);
+        $newTimestamp = CarbonImmutable::now()->timestamp;
+        $this->assertSame($previousTimestamp, $newTimestamp);
 
-        TestTime::addYear();
-        $this->assertSame('2020', (new CarbonImmutable())->format('Y'));
+        TestTime::unfreeze();
 
-        TestTime::addYear()->addYear();
-        $this->assertSame('2022', (new CarbonImmutable())->format('Y'));
+        sleep(2);
+        $newTimestamp = CarbonImmutable::now()->timestamp;
+        $this->assertNotEquals($previousTimestamp, $newTimestamp);
+
+
     }
 }
