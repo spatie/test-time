9c7e382651cf9bda7cc7227bd58a54f38da094df
Freek Van der Herten <freek@spatie.be>
2023-01-10 14:34:01 +0100

    wip

--- src/TestTime.php
+++ src/TestTime.php
@@ -157,6 +157,10 @@ class TestTime
             return Carbon::createFromFormat($args[0], $args[1]);
         }
 
+        if (count($args) === 3) {
+            return Carbon::createFromFormat($args[0], $args[1], $args[2]);
+        }
+
         throw new InvalidArgumentException('You can only pass a maximum of two arguments to `freeze`');
     }
 }
